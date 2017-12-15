9db15d97ee2ac361f4f084a63b96b7331352be8f
Erryl Alvares <erryl1@hotmail.com>
2017-12-15 17:29:49 +0530

    Add project files.

---
+++ PetaStarter/Controllers/CustomerController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cavala.Controllers
+{
+    public class CustomerController : EAController
+    {
+        // GET: Clients
+        public ActionResult Index(int? page, string PropName)
+        {
+            if (PropName?.Length > 0) page = 1;
+            return View("Index", base.BaseIndex<Customer>(page, "Customer where Name like '%" + PropName + "%'"));
+        }
+
+
+
+        // GET: Clients/Create
+        public ActionResult Manage(int? id)
+        {
+            return View(base.BaseCreateEdit<Customer>(id, "CustomerID"));
+        }
+
+        // POST: Customer/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Manage([Bind(Include = "CustomerID,Name,Address, PassportNo, DateIssue,DateExpiry")] Customer customer)
+        {
+            return base.BaseSave<Customer>(customer, customer.CustomerID > 0);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PetaStarter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cavala.MvcApplication" Language="C#" %>
